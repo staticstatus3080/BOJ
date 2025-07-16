513a2e2dfb68d35af239d439b7482cffa422553e
staticstatus3080 <parksuyong0308@gmail.com>
2025-07-16 16:33:33 +0900

    [Silver IV] Title: 영어 시험, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/25288. 영어 시험/영어 시험.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define X first 
+#define Y second 
+#define ll long long
+
+using namespace std;
+
+int main() {
+    cin.tie(0), ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    for (int i=0; i<n; i++) {
+        cout << s;
+    }
+} 
