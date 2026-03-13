a98704ace18b03bc29e88d0c4d621ae86eebb0a0
staticstatus3080 <parksuyong0308@gmail.com>
2026-03-13 23:17:29 +0900

    [Diamond V] Title: 균형잡힌 수열, Time: 8 ms, Memory: 12488 KB, Score: 26 point -BaekjoonHub

--- C++17/백준/Diamond/35341. 균형잡힌 수열/균형잡힌 수열.cc
+++ C++17/백준/Diamond/35341. 균형잡힌 수열/균형잡힌 수열.cc
@@ -28,51 +28,88 @@ bool lchk(int p, int dist) {
     return true;
 }
 
-void mpropagate(int p, int siz) {
+/*
+가운데에서 시작하는 전파는 chk가 끝날때 시작 (물론 배열 변화 없으면 안해도됨)
+경우 1(chk배열이 더 채워짐) : chk배열이 채워진 마지막 크기부터 전파
+경우 2(chk배열이 덜 채워짐) : 원래 chk배열이 채워진 마지막 크기부터 전파
+한마디로 chk배열이 달라지면 달라진 마지막 크기부터 전파
+*/
+// 0:양쪽전파, -1:왼전파, 1:오전파
+
+void rp(int p, int siz, int cnt) {
     int t = pow(2, siz+1)-1;
     int dist = (t+1)/2;
+    bool flag=0;
     if (lchk(p, dist)) {
         if (chk[siz][p-dist]>0 && chk[siz][p+dist]>0 && max(chk[siz][p-dist],chk[siz][p+dist])<V[p]) {
-            if (chk[siz+1][p]==0) ans++;
-            chk[siz+1][p] = V[p];
+            if (chk[siz+1][p]==0) {ans++; chk[siz+1][p] = V[p];}
+            else if (cnt==0)return;
+            else {
+                rp(p+dist, siz, 0);
+            }
         }
         else {
-            if (chk[siz+1][p]!=0) ans--;
-            chk[siz+1][p]=0;
+            if (chk[siz+1][p]!=0) {ans--; chk[siz+1][p]=0;}
+            else if (cnt==0) return;
+            else {
+                rp(p+dist, siz, 0);
+            }
         }
-        mpropagate(p, siz+1);
+        rp(p, siz+1, cnt+1);
     }
 }
-void lpropagate(int p, int siz) {
-    int t = pow(2, siz+1)-1; // 산맥 크기
+
+void lp(int p, int siz, int cnt) {
+    int t = pow(2, siz+1)-1;
     int dist = (t+1)/2;
+    bool flag=0;
     if (lchk(p, dist)) {
         if (chk[siz][p-dist]>0 && chk[siz][p+dist]>0 && max(chk[siz][p-dist],chk[siz][p+dist])<V[p]) {
-            if (chk[siz+1][p]==0) ans++;
-            chk[siz+1][p] = V[p];
+            if (chk[siz+1][p]==0) {ans++; chk[siz+1][p] = V[p];}
+            else if (cnt==0)return;
+            else {
+                rp(p-dist, siz, 0);
+            }
         }
         else {
-            if (chk[siz+1][p]!=0) ans--;
-            chk[siz+1][p]=0;
+            if (chk[siz+1][p]!=0) {ans--; chk[siz+1][p]=0;}
+            else if (cnt==0) return;
+            else {
+                rp(p-dist, siz, 0);
+            }
         }
-        lpropagate(p-dist, siz+1);
-        mpropagate(p, siz+1);
+        rp(p, siz+1, cnt+1);
     }
 }
-void rpropagate(int p, int siz) {
-    int t = pow(2, siz+1)-1; // 산맥 크기
+void mpropagate(int p, int siz, int dir) {
+    int t = pow(2, siz+1)-1;
     int dist = (t+1)/2;
+    bool flag=0;
     if (lchk(p, dist)) {
         if (chk[siz][p-dist]>0 && chk[siz][p+dist]>0 && max(chk[siz][p-dist],chk[siz][p+dist])<V[p]) {
             if (chk[siz+1][p]==0) ans++;
+            else flag=1;
             chk[siz+1][p] = V[p];
         }
         else {
             if (chk[siz+1][p]!=0) ans--;
+            else flag=1;
             chk[siz+1][p]=0;
         }
-        rpropagate(p+dist, siz+1);
-        mpropagate(p, siz+1);
+        if (flag) {
+            if (dir==0) {
+                lp(p-dist, siz, 0);
+                rp(p+dist, siz, 0);
+            }
+            else if (dir==1) {
+                rp(p+dist, siz, 0);
+            }
+            else {
+                lp(p-dist, siz, 0);
+            }
+            return;
+        }
+        mpropagate(p, siz+1, dir);
     }
 }
 //chk[a][b] : 크기가 a, 중앙이 b인 산맥의 값(산맥 안되면 0)
@@ -84,8 +121,8 @@ long long update_sequence(int p, int v) {
     양옆에 최소산맥부터 propagate
     아마도 중앙 propagate에서 전이되는건 따로해야할듯
     */
-    rpropagate(p+1, 0);
-    lpropagate(p-1, 0);
+    mpropagate(p+1, 0, 0);
+    mpropagate(p-1, 0, 0);
     int siz=0;
     /*
     중앙에서 mpropagate하다가 결과가 바뀌면 거기서 mp, lp, rp하고 끝
@@ -99,22 +136,23 @@ long long update_sequence(int p, int v) {
                 
                 if (chk[siz+1][p]==0) {
                     ans++;
+                    flag=1;
                 }
                 chk[siz+1][p] = V[p];
-                rpropagate(p+dist, siz+1);
-                lpropagate(p-dist, siz+1);
             }
             else {
-                while (chk[siz+1][p]>0) {
-                    chk[siz+1][p]=0;
+                if (chk[siz+1][p]>0) {
                     ans--;
-                    siz++;
+                    flag=1;
                 }
-                flag=1;
+                chk[siz+1][p] = 0;
             }
         }
         else break;
-        if (flag) break;
+        if (flag) {
+            mpropagate(p, siz, 0);
+            break;
+        }
         siz++;
     }
     return ans;
